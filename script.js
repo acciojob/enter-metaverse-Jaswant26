0bb191c3e9edd62afdbabac53a1c894c610eb407
Jaswant Singh Rawat <71880220+Jaswant26@users.noreply.github.com>
2023-10-23 17:07:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+ function enterMetaverse() {
+            // Change the text in the p tag to "Entered Metaverse"
+            document.getElementById("status").textContent = "Entered Metaverse";
+            
+            // Show the h1 tag that displays "Entered Metaverse"
+            document.getElementById("enteredStatus").style.display = "block";
+        }
